de45f447a25849b743fe735b45170bc102d6d8f3
Evandro Cavalcante Santos <evan.its.me@gmail.com>
2015-09-01 16:30:41 -0300

    Update exercise

--- aula-2.js
+++ aula-2.js
@@ -1,8 +1,21 @@
 
 // λx.λy UNICORNIO -> λy.UNICORNIO
 
-function(UNICORNIO){
+var fn = function(UNICORNIO){
 	return function(y){
-    	return UNICORNIO
+    	return UNICORNIO;
     }
 }
+
+fn(1);
+fn(1)(3);
+
+// λx.λy UNICORNIO -> λy. * UNICORNIO y
+var fn = function(UNICORNIO){
+    return function(y){
+        return UNICORNIO * y;
+    }
+}
+ 
+fn(1);
+fn(1)(3);
